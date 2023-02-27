f5dcdc6a8f62fbcdc6ad5f548c27ee60e9bca879
Estiven2004 <eospinaj@unal.edu.co>
2023-02-26 23:03:01 -0500

    Create Marca.java

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,15 @@
+package taller3.televisores;
+
+public class Marca{
+    private String nombre;
+
+    public Marca(String name){
+        this.nombre = name;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String Nombre){
+        this.nombre = Nombre;
+    }
+}
